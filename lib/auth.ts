44cc4f8887a267b10d8d1fb016d9fcaf4d5b98f3
Lee Seul <love.and.seul@gmail.com>
2025-09-10 13:26:32 +0900

    feat: credential login validator #1

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,12 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthError, type User } from "next-auth";
 import Credential from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Kakao from "next-auth/providers/kakao";
 import Naver from "next-auth/providers/naver";
+import z from "zod";
+import prisma from "./db";
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -17,38 +20,66 @@ export const {
     Github,
     Credential({
       credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "example@bookmark.com",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "pasword...",
-        },
+        email: {},
+        passwd: {},
       },
       async authorize(credentials) {
         console.log("credentials>>", credentials);
-        return null;
+        const { email, passwd } = credentials;
+        const validator = z
+          .object({
+            email: z.email("잘못된 이메일 형식입니다!"),
+            passwd: z.string().min(6, "More than 6 characters!"),
+          })
+          .safeParse({ email, passwd });
+
+        if (!validator.success) {
+          console.log("Error:", validator.error);
+          throw new AuthError(validator.error.message);
+        }
+        return { email, passwd } as User;
       },
     }),
   ],
   callbacks: {
-    async signIn({ user, profile }) {
+    async signIn({ user, profile, account }) {
+      const isCredential = account?.provider === "credentials";
+      const { email, name: nickname, image } = user;
+      if (!email) return false;
+
+      const mbr = await prisma.member.findUnique({ where: { email } });
+      console.log("💻 auth.ts ~ mbr:", mbr);
+
+      if (isCredential) {
+        if (!mbr) throw new AuthError("NotExistsMember");
+        // compare passwd ==> fail : error, success: login!
+      } else {
+        // sns login
+        if (!mbr && nickname) {
+          await prisma.member.create({
+            data: { email, nickname, image },
+          });
+        }
+      }
       return true;
     },
-    async jwt({ token, user }) {
+    async jwt({ token, user, trigger, account, session }) {
+      // console.log("💻 auth.ts ~ account:", account);
+      const userData = trigger === "update" ? session : user;
+
       // jwt 방식, GET /api/auth/callback/google에서는 없음
-      if (user) {
-        token.id = user.id;
-        token.email = user.email;
-        token.name = user.name;
+      if (userData) {
+        token.id = userData.id;
+        token.email = userData.email;
+        token.name = userData.name || userData.nickname;
       }
       return token;
     },
     async session({ session, token }) {
       if (token) {
+        session.user.id = token.id?.toString() || "";
+        session.user.name = token.name;
+        session.user.email = token.email as string;
       }
       return session;
     },
@@ -56,11 +87,10 @@ export const {
   trustHost: true,
   jwt: { maxAge: 30 * 60 },
   pages: {
-    signIn: `/sign`,
-    error: `/sign/error`,
+    signIn: "/sign",
+    error: "/sign/error",
   },
   session: {
     strategy: "jwt",
   },
-  secret: process.env.AUTH_SECRET as string,
 });
