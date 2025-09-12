595b6e1b0e964de029f0b1689d54cbf9d7d66841
Lee Seul <love.and.seul@gmail.com>
2025-09-12 20:46:25 +0900

    feat: add image utilities for signIn page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "*.googleusercontent.com" },
+      { hostname: "avatars.githubusercontent.com" },
+      { hostname: "phinf.pstatic.net" },
+      { hostname: "*.kakaocdn.net" },
+      { hostname: "localhost" },
+    ],
+  },
 };
 
 export default nextConfig;
