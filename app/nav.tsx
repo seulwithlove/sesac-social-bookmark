ebb9107d6fa8c5d12855520edb36ef6003576ef7
Lee Seul <love.and.seul@gmail.com>
2025-09-12 20:42:17 +0900

    feeat: set profile image in nav for logged-in users

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,8 +1,10 @@
 import { SquareLibraryIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { use } from "react";
 import ThemeChanger from "@/components/theme-changer";
 import { auth } from "@/lib/auth";
+import DummyProfile from "@/public/profile-dummy.png";
 
 export default function Nav() {
   const session = use(auth());
@@ -14,8 +16,13 @@ export default function Nav() {
       </Link>
       <ThemeChanger />
       {didLogin ? (
-        <Link href="/my" className="">
-          {session.user?.name}
+        <Link href="/my" className="overflow-hidden rounded-full">
+          <Image
+            src={DummyProfile}
+            alt={session.user?.name || "guest"}
+            width={40}
+            height={40}
+          />
         </Link>
       ) : (
         <Link href="/sign">Login</Link>
