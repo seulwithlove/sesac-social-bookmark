d618f52ff8140edf40afd835fc6fa9fd03475c9e
Lee Seul <love.and.seul@gmail.com>
2025-09-25 17:36:01 +0900

    feat: add unique id generator

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,6 +8,17 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// nanoid library도 사용가능
+export const uniqNumId = (cnt = 5) =>
+  Math.random()
+    .toString(10)
+    .substring(2, cnt + 2);
+
+export const uniqId = (cnt = 5) =>
+  Math.random()
+    .toString(36)
+    .substring(2, cnt + 2);
+
 export const DynamicCsses = [
   "translate-x-[-20px]",
   "translate-x-[-40px]",
