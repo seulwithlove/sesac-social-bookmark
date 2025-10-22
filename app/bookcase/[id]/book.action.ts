972f3ded9d370cae1a0275d6b2cce1b2f9d1e1c6
Lee Seul <love.and.seul@gmail.com>
2025-10-22 16:15:30 +0900

    feat: add CRUD of book

---
+++ app/bookcase/[id]/book.action.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/db";
+import { validate } from "@/lib/validator";
+import z from "zod";
+
+export const saveBook = async (formData: FormData) => {
+  const session = await auth();
+  if (!session?.user || !session.user.id) throw new Error("Need Login");
+
+  const member = Number(session.user.id);
+
+  const zobj = z
+    .object({
+      title: z.string().min(1),
+      ispublic: z.string().optional(),
+      withdel: z.string().optional(),
+      remark: z.string().optional(),
+    })
+    .refine(({ ispublic, withdel }) => !ispublic || (ispublic && !withdel), {
+      path: ["withdel"],
+      message: "Public book cannot have opening with deletion",
+    });
+
+  const [err, data] = validate(zobj, formData);
+  console.log("💻 - book.action.ts - err, data:", err, data);
+
+  if (err) return err;
+
+  const id = Number(formData.get("id"));
+  if (id) {
+    await prisma.book.update({
+      where: { id },
+      data: {
+        ...data,
+        ispublic: data.ispublic === "on",
+        withdel: !!data.withdel,
+      },
+    });
+  } else {
+    await prisma.book.create({
+      data: {
+        ...data,
+        ispublic: data.ispublic === "on",
+        withdel: !!data.withdel,
+        member,
+      },
+    });
+  }
+};
+
+export const deleteBook = async (id: number) =>
+  prisma.book.delete({
+    where: { id },
+  });
